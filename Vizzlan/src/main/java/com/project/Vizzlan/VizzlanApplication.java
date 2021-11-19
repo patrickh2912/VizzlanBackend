bcbc88fe3245c4a03251c170da9419186d478125
Patrick Huillca <patrick.h291299@gmail.com>
2021-11-19 02:37:10 -0500

    Primer commit

---
+++ Vizzlan/src/main/java/com/project/Vizzlan/VizzlanApplication.java
@@ -0,0 +1,13 @@
+package com.project.Vizzlan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VizzlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VizzlanApplication.class, args);
+	}
+
+}
